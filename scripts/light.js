f8875379387280927d50d033a3695c3283819394
Edward Kim <onward.edkim@gmail.com>
2016-10-30 22:39:52 +1100

    add request

--- scripts/light.js
+++ scripts/light.js
@@ -5,6 +5,8 @@
 //   lizbot <위치> (조명|불|등)(켜|꺼) - <위치>의 조명을 조작합니다
 //
 
+const request = require('request')
+
 const URI = process.env.LIGHT_API_URI || 'http://localhost:5000'
 
 const alias = {
@@ -20,15 +22,16 @@ const actions = {
 
 module.exports = function (robot) {
   robot.hear(/(.*) (조명|불|등)(.*)(켜|꺼|끄)/i, (res) => {
-    console.log(res.match)
     const key = res.match[1]
     const target = res.match[2]
     const action = res.match[4]
     const location = alias[key] || key
     const status = actions[action] || action
 
-    res.http(`${URI}/op/${location}`)
-      .post({ status })((err, response, body) => {
+    request.post({
+      url: `${URI}/op/${location}`,
+      form: { status }
+    }, (err, response, body) => {
         if(!body || err) {
           return res.send('동작에 문제가 있습니다.')
         }
