a60950c79c900d643dd7dd095580ef559c3918b3
Edward Kim <onward.edkim@gmail.com>
2016-10-30 23:08:20 +1100

    update light

--- scripts/light.js
+++ scripts/light.js
@@ -8,6 +8,7 @@
 const request = require('request')
 
 const URI = process.env.LIGHT_API_URI || 'http://localhost:5000'
+const DURATION = 20
 
 const alias = {
   '거실': 'living',
@@ -43,21 +44,31 @@ module.exports = function (robot) {
     const key = res.match[1]
     const target = res.match[2]
     const action = res.match[4]
-    const location = alias[key] || key
-    const status = actions[action] || action
+    operate(res, key, action, (result) => {
+      if (result.okay) {
+        res.send(`${key} ${target}이 ${action}졌습니다.`)
+      }
+    })
+  })
 
-    request.post({
-      url: `${URI}/op/${location}`,
-      form: { status }
-    }, (err, response, body) => {
-        if(!body || err) {
-          return res.send('동작에 문제가 있습니다.')
-        }
+  robot.hear(/자러(.*)(가|간)/i, (res) => {
+    operate(res, 'bed', 'on', (result) => {
+      if (result.okay) {
+        res.send(`침실 등이 켜졌습니다. ${DURATION}초 후에 거실 등이 꺼집니다.`)
+        setTimeout(
+          () => operate(res, 'living', 'off'),
+          DURATION * 1000 // Super inaccurate time
+        )
+      }
+    })
+  })
 
-        let result = JSON.parse(body)
-        if(result.okay) {
-          res.send(`${key} ${target}이 ${action}졌습니다.`)
-        }
-      })
+  robot.hear(/(잔다|자라|주무세요)/i, (res) => {
+    res.send(`거실 소등 합니다. ${DURATION}초 후에 침실 등이 꺼집니다. 주무세요~`)
+    operate(res, 'living', 'off')
+    setTimeout(
+      () => operate(res, 'bed', 'off'),
+      DURATION * 1000 // Super inaccurate time
+    )
   })
 }
